c5ec56ba32380ba1a8c14a7a7121e7346b86b57c
vamsireddy1912 <kapuramvamsi1912@gmail.com>
2023-11-09 22:32:53 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+def pushpa(request):
+    return render(request,'pushpa.html')
+
+
+def rolex(request):
+    return render(request,'rolex.html')
+
+
+def dharani(request):
+    return render(request,'dharani.html')
+
+def bigil(request):
+    return render(request,'bigil.html')
